6f9d6c9f5121134a81796bddf980a6ec2ca9cd4f
hisoka <hisoka>
2026-02-03 14:07:38 +0100

    Implement home page with TradingView widgets

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,39 +1,49 @@
 "use clien";
+import useTradingViewWidget from "@/hooks/useTradingViewWidget";
+import { cn } from "@/lib/utils";
 // TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from "react";
+import React, { useRef, memo } from "react";
 
-const TradingViewWidget = () => {
-  const container = useRef(null);
+interface TradingViewWidgetProps {
+  title?: string;
+  scriptUrl: string;
+  config: Record<string, unknown>;
+  height?: number;
+  className?: string;
+}
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = ``;
-    container.current.appendChild(script);
-  }, []);
+const TradingViewWidget = ({
+  title,
+  scriptUrl,
+  config,
+  height = 600,
+  className,
+}: TradingViewWidgetProps) => {
+  const container = useTradingViewWidget(scriptUrl, config, height);
 
   return (
-    <div
-      className="tradingview-widget-container"
-      ref={container}
-      style={{ height: "100%", width: "100%" }}
-    >
+    <div className="w-full">
+      {title && (
+        <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>
+      )}
       <div
-        className="tradingview-widget-container__widget"
-        style={{ height: "calc(100% - 32px)", width: "100%" }}
-      ></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/symbols/NASDAQ-AAPL/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">AAPL stock chart</span>
-        </a>
-        <span className="trademark"> by TradingView</span>
+        className={cn("tradingview-widget-container", className)}
+        ref={container}
+      >
+        <div
+          className="tradingview-widget-container__widget"
+          style={{ height, width: "100%" }}
+        />
+        {/* <div className="tradingview-widget-copyright">
+          <a
+            href="https://www.tradingview.com/symbols/NASDAQ-AAPL/"
+            rel="noopener nofollow"
+            target="_blank"
+          >
+            <span className="blue-text">AAPL stock chart</span>
+          </a>
+          <span className="trademark"> by TradingView</span>
+        </div> */}
       </div>
     </div>
   );
